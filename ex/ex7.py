51550eade2473d3785a4ef78abdbd0ae87df5b44
mason seibert <masonrseibert@gmail.com>
2021-08-31 11:56:16 -0400

    Morning exercises

---
+++ ex/ex7.py
@@ -0,0 +1,23 @@
+print ("Mary had a little lamp.")
+print ("Its light was white as %s." % 'light')
+print ("And every where that Mary went.")
+print ("." * 10) # what did this do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# to get two print functions to be on same line when print.
+# comma at end, and use function end = 'whatever you want between here'
+
+print (end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+print (end7 + end8 + end9 + end10 + end11 + end12)
