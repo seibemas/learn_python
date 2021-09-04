40086cd36a4af45beaa1dcf5def1aecdb115fca1
mason seibert <masonrseibert@gmail.com>
2021-09-04 05:54:03 -0400

    work for 9/3

---
+++ ex/ex11.py
@@ -0,0 +1,10 @@
+print ("How old are you?"),
+age = input()
+print ("How tall are you?"),
+height = input()
+print ("How much do you weigh?"),
+weight = input()
+
+# raw_input() changed to just input() in python 3.
+
+print ("So, you're %r years old, %r tall and %r heavy." % (age, height, weight))
